2db3500f2fb2b92442f1d626e444f6a481986a1c
Tom Rankin <tom@tomrankin.net>
2019-06-05 09:23:46 -0500

    refactor

--- webworker/1.js
+++ webworker/1.js
@@ -3,13 +3,13 @@ function someLongRunningTask(ms){
 }
 
 console.log("Going to kick off that task.");
-var myWorker = new Worker('worker.js');
+var myWorker = new Worker('workerasync.js');
 myWorker.onmessage = (e) => {
     console.log('Message received from worker: ' + JSON.stringify(e.data));
 }
 someLongRunningTask(400);
 someLongRunningTask(100);
-someLongRunningTask(450);
+someLongRunningTask(2050);
 someLongRunningTask(150);
 console.log("Fired It Off");
 console.log("DONE");
